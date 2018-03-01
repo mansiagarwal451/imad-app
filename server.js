1a3e373642656f1e003ff3904be1b534c59dd372
Mansi Agarwal <mansiagarwal451@users.noreply.github.com>
2018-03-02 04:31:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,51 @@ var articleOne = {
    Heading: 'Article-one',
    Date: 'Mar 1, 2018',
    content: 
-           '<p>
+           `<p>
                 This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article.
             </p>
             <p>
                 This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article.
-            </p>'
+            </p>`
    
    
 };
+
+function createTemplate(data){
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    
+
+var htmlTemplate = 
+<html>
+    <head>
+        <title>
+            $(title)
+        </title>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+         <div>
+            <a href="/">Home</a>
+         </div>
+         <hr/>
+         <h3>
+            $(heading)
+         </h3>
+         <div>
+            $(date)
+         </div>
+         <div>
+           $(content)
+         </div>
+        </div>
+    </body>
+    
+</html>
+return htmlTemplate;
 
     
 
@@ -27,7 +63,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two',function(req,res){
